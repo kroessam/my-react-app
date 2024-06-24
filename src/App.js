f5c64fdd8391c0802822fb20c01f7770920fa989
Samuel Kroes <kroessamuel@gmail.com>
2024-06-24 15:04:20 -0700

    Click Event Updates State

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
+import { useState } from "react";
+
 // Receives "value" prop from Board to display
-function Square({ value }) {
-  return <button className="square">{value}</button>;
+function Square() {
+  const [value, setValue] = useState(null);
+
+  function handleClick() {
+    setValue("X"); // calling "set" function rerenders component
+  }
+
+  return (
+    <button className="square" onClick={handleClick}>
+      {value}
+    </button>
+  );
 }
 
 /* keywords */
@@ -11,19 +23,19 @@ export default function Board() {
   return (
     <>
       <div className="board-row">
-        <Square value="1" />
-        <Square value="2" />
-        <Square value="3" />
+        <Square />
+        <Square />
+        <Square />
       </div>
       <div className="board-row">
-        <Square value="4" />
-        <Square value="5" />
-        <Square value="6" />
+        <Square />
+        <Square />
+        <Square />
       </div>
       <div className="board-row">
-        <Square value="7" />
-        <Square value="8" />
-        <Square value="9" />
+        <Square />
+        <Square />
+        <Square />
       </div>
     </>
   );
