9bf3a238bad184716c97f5da44c17ddb4884452d
Samuel Kroes <kroessamuel@gmail.com>
2024-06-25 15:24:28 -0700

    Game Functional!

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ export default function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
 
   function handleClick(i) {
-    if (squares[i]) {
+    if (squares[i] || calculateWinner(squares)) {
       return; // do not place a tile if square is occupied
     }
 
@@ -26,16 +26,25 @@ export default function Board() {
     if (xIsNext) {
       newSquares[i] = "X";
     } else {
-      newSquares[i] = "O"
+      newSquares[i] = "O";
     }
-    
+
     setSquares(newSquares); // reassign to squares variable
     setXIsNext(!xIsNext);
+  }
+
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = "Winner: " + winner;
+  } else {
+    status = "Next player: " + (xIsNext ? "X" : "O");
   }
 
   // must return all JSX within one parent tag (<></> is a "fragment")
   return (
     <>
+      <div className="status">{status}</div>
       <div className="board-row">
         <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
         <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
@@ -53,4 +62,26 @@ export default function Board() {
       </div>
     </>
   );
+}
+
+// Determine whether there are 3-in-a-row of "X" or "O"
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
