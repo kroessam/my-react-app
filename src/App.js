e4dd364ded4776558bd93bf5ced63345a170abf3
Samuel Kroes <kroessamuel@gmail.com>
2024-06-25 14:58:49 -0700

    Squares' State Held in Board

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
 import { useState } from "react";
 
 // Receives "value" prop from Board to display
-function Square() {
-  const [value, setValue] = useState(null);
-
-  function handleClick() {
-    setValue("X"); // calling "set" function rerenders component
-  }
-
+function Square({ value, onSquareClick }) {
   return (
-    <button className="square" onClick={handleClick}>
+    <button className="square" onClick={onSquareClick}>
       {value}
     </button>
   );
@@ -19,23 +13,31 @@ function Square() {
 // "export": lets other files access this function
 // "default": tells other files this is the main function
 export default function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
+  function handleClick() {
+    const newSquares = squares.slice();  // create a copy of squares array
+    newSquares[0] = "X";
+    setSquares(newSquares);
+  }
+
   // must return all JSX within one parent tag (<></> is a "fragment")
   return (
     <>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[0]} onSquareClick={handleClick} />
+        <Square value={squares[1]} onSquareClick={handleClick} />
+        <Square value={squares[2]} onSquareClick={handleClick} />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[3]} onSquareClick={handleClick} />
+        <Square value={squares[4]} onSquareClick={handleClick} />
+        <Square value={squares[5]} onSquareClick={handleClick} />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[6]} onSquareClick={handleClick} />
+        <Square value={squares[7]} onSquareClick={handleClick} />
+        <Square value={squares[8]} onSquareClick={handleClick} />
       </div>
     </>
   );
