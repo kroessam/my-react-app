75efab0820116ed7d788bff2f68a9aa3909cd374
Samuel Kroes <kroessamuel@gmail.com>
2024-07-15 15:26:39 -0700

    State Updating On Click

--- mosh-tutorial/src/components/ListGroup.tsx
+++ mosh-tutorial/src/components/ListGroup.tsx
@@ -1,11 +1,8 @@
-import { MouseEvent } from "react";
+import { useState } from "react";
 
 function ListGroup() {
   let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
-  //items = [];
-
-  // Event Handler
-  const handleClick = (event: MouseEvent) => console.log(event);
+  const [selectedIndex, setSelectedIndex] = useState(-1);
 
   return (
     <>
@@ -13,7 +10,17 @@ function ListGroup() {
       {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
         {items.map((item, index) => (
-          <li key={item} className="list-group-item" onClick={handleClick}>
+          <li
+            key={item}
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            onClick={() => {
+              setSelectedIndex(index);
+            }}
+          >
             {item}
           </li>
         ))}
