79296e1551539004985422aa06b4d58744f9f271
Samuel Kroes <kroessamuel@gmail.com>
2024-07-15 15:13:14 -0700

    Onclick Displays Event Target

--- mosh-tutorial/src/components/ListGroup.tsx
+++ mosh-tutorial/src/components/ListGroup.tsx
@@ -1,14 +1,19 @@
+import { MouseEvent } from "react";
+
 function ListGroup() {
   let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
-  items = [];
+  //items = [];
+
+  // Event Handler
+  const handleClick = (event: MouseEvent) => console.log(event);
 
   return (
     <>
       <h1>List</h1>
       {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
-        {items.map((item) => (
-          <li key={item} className="list-group-item">
+        {items.map((item, index) => (
+          <li key={item} className="list-group-item" onClick={handleClick}>
             {item}
           </li>
         ))}
