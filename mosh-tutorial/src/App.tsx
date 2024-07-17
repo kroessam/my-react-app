15dd556196d4569ad7e0d66426abbad6a718cf1a
Samuel Kroes <kroessamuel@gmail.com>
2024-07-17 13:40:50 -0700

    Button Component Completed

--- mosh-tutorial/src/App.tsx
+++ mosh-tutorial/src/App.tsx
@@ -1,9 +1,13 @@
-import Alert from "./components/Alert";
+import Button from "./components/Button";
 
 function App() {
+  const handleClick = () => {
+    console.log("Button clicked!");
+  };
+
   return (
     <div>
-      <Alert>Hello <span>World</span></Alert>
+      <Button color="primary" onClick={handleClick}>My Button</Button>
     </div>
   );
 }
