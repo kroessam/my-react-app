4d7f0d917801123298db66db5d87fd219b895e84
Samuel Kroes <kroessamuel@gmail.com>
2024-07-24 15:12:15 -0700

    Counter State Extracted

--- sample-site/src/App.tsx
+++ sample-site/src/App.tsx
@@ -1,24 +1,14 @@
 import Title from "./components/Title";
 import Navbar from "./components/Navbar";
-import Counter from "./components/Counter";
 import Combiner from "./components/Combiner";
-import { useState } from "react";
 
 function App() {
-  const [num1, setNum1] = useState(10);
-  const [num2, setNum2] = useState(-5);
-
-
   return (
     <>
       <Navbar />
       <main className="container mt-5">
         <Title text="My React App Using TypeScript" />
-        <div className="d-flex justify-content-around">
-          <Counter header="1st Number" amt={num1} />
-          <Counter header="2nd Number" amt={num2} />
-          <Combiner />
-        </div>
+        <Combiner />
       </main>
     </>
   );
--- sample-site/src/components/Counter.tsx
+++ sample-site/src/components/Counter.tsx
@@ -2,22 +2,31 @@ import { useState } from "react";
 
 interface Props {
   header?: string;
-  amt?: number;
+  incrementAmt?: number;
+  onIncrement: (incrementAmt: number) => void;
 }
 
-const Counter = ({header = "Counter", amt = 0}: Props) => {
-  const [count, setCount] = useState(0);
-  let btnText = amt >= 0 && "+";
+const Counter = ({
+  header = "Counter",
+  incrementAmt = 0,
+  onIncrement,
+}: Props) => {
+  const [count, setCount] = useState(0); // track counter's value
+  let btnText = incrementAmt >= 0 && "+"; // Add  the plus sign (+) if positive
 
   return (
     <div className="w-25 d-flex flex-column align-items-center">
       <h3>{header}</h3>
-      <p>{count}</p>
+      <p className="fs-2">{count}</p>
       <button
         className="btn btn-outline-dark"
-        onClick={() => setCount(count + amt)}
+        onClick={() => {
+          setCount(count + incrementAmt);
+          onIncrement(count);
+        }}
       >
-        {btnText}{amt}
+        {btnText}
+        {incrementAmt}
       </button>
     </div>
   );
