bc685b2e26cb62fb7ca38ee0c5f851d1b8be16b1
Samuel Kroes <kroessamuel@gmail.com>
2024-07-28 11:11:27 -0700

    URLSearch Component Created

---
+++ sample-site/src/components/URLSearch.tsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
+const URLSearch = () => {
+  const [input, setInput] = useState("");
+
+  return (
+    <div className="text-center">
+      <h3 className="mb-4">Test This App's Routing</h3>
+      <p>Enter a path to try, then press "go"! (Leave blank to go home)</p>
+      <div className="input-group w-25 mx-auto mb-2">
+        <span className="input-group-text">/</span>
+        <input
+          className="form-control w-25 mx-auto"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+      </div>
+      <p>
+        Going to: <span className="text-success">/{input}</span>
+      </p>
+      <Link className="btn btn-primary" to={input}>
+        Go
+      </Link>
+    </div>
+  );
+};
+
+export default URLSearch;
--- sample-site/src/pages/PageNotFound.tsx
+++ sample-site/src/pages/PageNotFound.tsx
@@ -1,11 +1,18 @@
 import { Link } from "react-router-dom";
+import URLSearch from "../components/URLSearch";
 
 export const PageNotFound = () => {
   return (
-    <div className="text-center mt-5">
+    <div>
+      <div className="text-center my-5">
       <h1 className="display-1">404</h1>
       <p className="lead">Error: Page Not Found</p>
-      <Link className="btn btn-outline-dark" to="/">Back to Home</Link>
+      <Link className="btn btn-outline-dark" to="/">
+        Back to Home
+      </Link>
+      </div>
+      <hr className="mb-5"/>
+      <URLSearch />
     </div>
   );
 };
